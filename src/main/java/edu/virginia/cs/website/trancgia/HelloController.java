d10cec040ba8a05b34ea3ca3e07ad1f7503c801b
Christopher Tran <chrisgktran@gmail.com>
2024-05-15 20:20:44 -0400

    add

---
+++ src/main/java/edu/virginia/cs/website/trancgia/HelloController.java
@@ -0,0 +1,14 @@
+package edu.virginia.cs.website.trancgia;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
